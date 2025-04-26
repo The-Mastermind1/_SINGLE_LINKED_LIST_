056e2b0ba9547949f3430b69cb50e995489bf79d
panagiotis dimitropoulos <162046043+The-Mastermind1@users.noreply.github.com>
2025-04-26 21:10:27 +0300

    Update Macros.h

--- Macros.h
+++ Macros.h
@@ -1,4 +1,4 @@
-#pragma once
+
 #pragma once
 #include<exception>
 #include<string>
@@ -45,37 +45,7 @@ public:
     }
 };
 
-class pop_from_an_empty_queue_ :public std::exception
-{
-private:
-    std::string errorMessage; // To store the error message
-public:
-    //Constructor to initialize the error message
-    explicit pop_from_an_empty_queue_(const std::string& message)
-        : errorMessage(message) {
-    }
 
-    // Override the what() method
-    const char* what() const noexcept override {
-        return errorMessage.c_str();
-    }
-};
-
-class tried_to_access_an_empty_queue_ :public std::exception
-{
-private:
-    std::string errorMessage; // To store the error message
-public:
-    //Constructor to initialize the error message
-    explicit tried_to_access_an_empty_queue_(const std::string& message)
-        : errorMessage(message) {
-    }
-
-    // Override the what() method
-    const char* what() const noexcept override {
-        return errorMessage.c_str();
-    }
-};
 
 //begin
 template<typename _Ty>
